1edc49bb63c107051c178ab616d79c952117e34d
viku5211 <126612558+viku5211@users.noreply.github.com>
2024-01-24 18:50:23 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -41,21 +41,21 @@ data.data.map(function(item){
 export default function decorate(block) {
      console.log("ssss aaAaAAaaA", block);
   // var courses = getData(block);
-  //   console.log("ssss aaAaAAaaA", block);
-  // /* change to ul, li */
-  // // console.log("test",block);
-  // const ul = document.createElement('ul');
-  // [...block.children].forEach((row) => {
-  //   const li = document.createElement('li');
-  //   while (row.firstElementChild) li.append(row.firstElementChild);
-  //   [...li.children].forEach((div) => {
-  //     if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-  //     else div.className = 'cards-card-body';
-  //   });
-  //   ul.append(li);
-  // });
-  // ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  // block.textContent = '';
-  // block.append(ul);
+    console.log("ssss aaAaAAaaA", block);
+  /* change to ul, li */
+  // console.log("test",block);
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
   //   console.log("test1",block);
 }
