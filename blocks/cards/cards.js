323aecac9ec926988e63523ab052536f94f88c0d
viku5211 <126612558+viku5211@users.noreply.github.com>
2024-01-24 15:48:47 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -15,6 +15,12 @@ const getData = () => {
     ).then((response) => {
       response.json().then((data) => {
           res = data;
+          if(data.data){
+data.data.map(function(item){
+    console.log(item);
+    var content = '<li><div class="cards-card-image"><picture><source type="image/webp" srcset="/media_1fa6f697de17429a7e9d0a36e5cf954884071248f.jpeg?width=750&amp;format=webply&amp;optimize=medium"><img loading="lazy" alt="An iceberg" src="/media_1fa6f697de17429a7e9d0a36e5cf954884071248f.jpeg?width=750&amp;format=jpeg&amp;optimize=medium"></picture></div><div class="cards-card-body"><p><strong>Content at scale</strong></p><p>AEM allows you to publish more content in shorter time with smaller teams</p><p>https://github.com/viku5211/edge/blob/main/blocks/cards/course.json</p></div></li>';
+});
+          }
         
       });
     });
