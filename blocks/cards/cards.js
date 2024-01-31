14297d1e22d5f28b8427dc4e8ab2e22dcbaccc5f
viku5211 <126612558+viku5211@users.noreply.github.com>
2024-01-31 14:59:47 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,48 +1,18 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
-  // Make an HTTP request to fetch the JSON data
-  fetch('https://raw.githubusercontent.com/viku5211/edge/main/blocks/cards/course.json')
-    .then(response => response.json())
-    .then(data => {
-      const courses = data.courses;
-
-      const ul = document.createElement('ul');
-      ul.style.listStyle = 'none';
-      ul.style.margin = '0';
-      ul.style.padding = '0';
-      ul.style.display = 'grid';
-      ul.style.gridTemplateColumns = 'repeat(auto-fill, minmax(278px, 1fr))';
-      ul.style.gridGap = '16px';
-
-      courses.forEach(course => {
-        const li = document.createElement('li');
-        li.style.border = '1px solid var(--dark-color)';
-        li.style.backgroundColor = 'var(--background-color)';
-        li.style.margin = '16px';
-
-        const title = document.createElement('h2');
-        title.textContent = course.name;
-
-        const description = document.createElement('p');
-        description.textContent = course.description;
-
-        const image = document.createElement('img');
-        image.src = course.image;
-        image.alt = course.name;
-        image.style.width = '100%';
-        image.style.aspectRatio = '4 / 3';
-        image.style.objectFit = 'cover';
-
-        li.appendChild(title);
-        li.appendChild(description);
-        li.appendChild(image);
-
-        ul.appendChild(li);
-      });
-
-      block.textContent = '';
-      block.appendChild(ul);
-    })
-    .catch(error => console.error(error));
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
 }
